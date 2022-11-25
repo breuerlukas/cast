2b200305d55b99ae97bbc663a877891fb9088a61
DerCoderLukas <lukas.dercoder@gmail.com>
2022-11-25 14:45:03 +0100

    Implemented IndicatorRepository

---
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/indicator/IndicatorRepository.java
@@ -0,0 +1,32 @@
+package de.lukasbreuer.stockalgorithm.core.neuralnetwork.dataset.indicator;
+
+import com.clearspring.analytics.util.Lists;
+import de.lukasbreuer.stockalgorithm.core.symbol.HistoryEntry;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class IndicatorRepository {
+  private final List<HistoryEntry> data;
+  private final List<Indicator> indicators = Lists.newArrayList();
+
+  public void fill() {
+    indicators.add(ChangeIndicator.create(data));
+    indicators.add(MovingAverageIndicator.create(data));
+    indicators.add(ChangeRateIndicator.create(data));
+    indicators.add(RelativeStrengthIndicator.create(data));
+    indicators.add(CommodityChannelIndicator.create(data));
+    indicators.add(StochasticOscillatorIndicator.create(data));
+    indicators.add(AverageRangeIndicator.create(data));
+    indicators.add(BullishPatternIndicator.create(data));
+    indicators.add(BearishPatternIndicator.create(data));
+  }
+
+  public <T extends Indicator> T find(Class<T> type) {
+    return (T) indicators.stream()
+      .filter(indicator -> indicator.getClass() == type)
+      .collect(Collectors.toList());
+  }
+}
