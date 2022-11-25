62851e77e9d6ed9a2fb08b828bff0a49986ad0da
DerCoderLukas <lukas.dercoder@gmail.com>
2022-11-25 18:19:21 +0100

    Updated StockDataset

--- core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/StockDataset.java
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/StockDataset.java
@@ -2,22 +2,61 @@ package de.lukasbreuer.stockalgorithm.core.neuralnetwork.dataset;
 
 import com.clearspring.analytics.util.Lists;
 import de.lukasbreuer.stockalgorithm.core.neuralnetwork.ModelState;
+import de.lukasbreuer.stockalgorithm.core.neuralnetwork.dataset.indicator.IndicatorRepository;
+import de.lukasbreuer.stockalgorithm.core.symbol.HistoryEntry;
 import de.lukasbreuer.stockalgorithm.core.symbol.Symbol;
 import de.lukasbreuer.stockalgorithm.core.trade.TradeType;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor(staticName = "create")
 public final class StockDataset {
   private final Symbol symbol;
   private final TradeType tradeType;
   private final ModelState modelState;
+  private final int trainPeriod;
+  private final int evaluationPeriod;
+  private final int reviewPeriod;
   private final List<Map.Entry<List<double[]>, Double>> dataset = Lists.newArrayList();
+  private List<HistoryEntry> historyData;
+  private List<DatasetDay> dayData;
 
   public void build() {
+    historyData = createHistoryData();
+    var indicatorRepository = IndicatorRepository.create(historyData);
+    dayData = createDayData(indicatorRepository);
+  }
 
+  private List<HistoryEntry> createHistoryData() {
+    var data = symbol.findPartOfHistory(trainPeriod + evaluationPeriod);
+    if (modelState == ModelState.TRAINING) {
+      data = data.stream().limit(trainPeriod).collect(Collectors.toList());
+    }
+    if (modelState == ModelState.EVALUATING) {
+      data = data.stream().skip(evaluationPeriod).collect(Collectors.toList());
+    }
+    return data;
+  }
+
+  private List<DatasetDay> createDayData(IndicatorRepository indicatorRepository) {
+    var data = Lists.<DatasetDay>newArrayList();
+    for (var i = 0; i < historyData.size(); i++) {
+      var day = DatasetDay.create(i, indicatorRepository);
+      day.build();
+      data.add(day);
+    }
+    return data;
+  }
+
+  private List<double[]> createInputData(int index) {
+    var inputData = Lists.<double[]>newArrayList();
+    for (var i = 0; i < reviewPeriod; i++) {
+      inputData.add(dayData.get(index - i).raw());
+    }
+    return inputData;
   }
 
   public List<Map.Entry<List<double[]>, Double>> raw() {
