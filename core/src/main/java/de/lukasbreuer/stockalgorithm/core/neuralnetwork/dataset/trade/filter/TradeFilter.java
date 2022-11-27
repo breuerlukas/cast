d4a1ddafa8b7ae49e9e373315912336e7b53c86d
DerCoderLukas <lukas.dercoder@gmail.com>
2022-11-27 18:25:23 +0100

    Implemented TradeIntersectionFilter

--- core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/trade/filter/TradeFilter.java
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/trade/filter/TradeFilter.java
@@ -2,12 +2,16 @@ package de.lukasbreuer.stockalgorithm.core.neuralnetwork.dataset.trade.filter;
 
 import de.lukasbreuer.stockalgorithm.core.trade.Trade;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
+@Accessors(fluent = true)
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class TradeFilter {
+  @Getter(AccessLevel.PROTECTED)
   private final List<Trade> initialTrades;
 
   public abstract List<Trade> filter();
--- core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/trade/filter/TradeIntersectionFilter.java
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/dataset/trade/filter/TradeIntersectionFilter.java
@@ -1,5 +1,6 @@
 package de.lukasbreuer.stockalgorithm.core.neuralnetwork.dataset.trade.filter;
 
+import com.clearspring.analytics.util.Lists;
 import de.lukasbreuer.stockalgorithm.core.trade.Trade;
 
 import java.util.List;
@@ -15,6 +16,63 @@ public final class TradeIntersectionFilter extends TradeFilter {
 
   @Override
   public List<Trade> filter() {
-    return null;
+    var qualifiedTrades = Lists.<Trade>newArrayList();
+    var excludedTrades = Lists.<Trade>newArrayList();
+    for (var i = 0; i < initialTrades().size(); i++) {
+      var trade = initialTrades().get(i);
+      if (excludedTrades.contains(trade)) {
+        continue;
+      }
+      qualifiedTrades.add(trade);
+      crosscheckTrade(trade, qualifiedTrades, excludedTrades);
+    }
+    return qualifiedTrades;
+  }
+
+  private void crosscheckTrade(
+    Trade trade, List<Trade> qualifiedTrades, List<Trade> excludedTrades
+  ) {
+    for (var j = 0; j < initialTrades().size(); j++) {
+      var reviewedTrade = initialTrades().get(j);
+      if (reviewedTrade.equals(trade) || excludedTrades.contains(reviewedTrade)) {
+        continue;
+      }
+      if (isTradeIntersectionPresent(trade, reviewedTrade)) {
+        excludedTrades.add(reviewedTrade);
+        qualifiedTrades.remove(reviewedTrade);
+      }
+    }
+  }
+
+  private boolean isTradeIntersectionPresent(Trade trade, Trade reviewedTrade) {
+    if (isEnclosedIn(trade, reviewedTrade)) {
+      return true;
+    }
+    if (allowsEarlierBuyTime(trade, reviewedTrade)) {
+      trade.buyTime(reviewedTrade.buyTime());
+      return true;
+    }
+    if (allowsLaterSellTime(trade, reviewedTrade)) {
+      trade.sellTime(reviewedTrade.sellTime());
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isEnclosedIn(Trade trade, Trade reviewedTrade) {
+    return reviewedTrade.buyTime() >= trade.buyTime() &&
+      reviewedTrade.sellTime() <= trade.sellTime();
+  }
+
+  private boolean allowsEarlierBuyTime(Trade trade, Trade reviewedTrade) {
+    return reviewedTrade.sellTime() >= trade.buyTime() &&
+      reviewedTrade.sellTime() <= trade.sellTime() &&
+      reviewedTrade.buyTime() <= trade.buyTime();
+  }
+
+  private boolean allowsLaterSellTime(Trade trade, Trade reviewedTrade) {
+    return reviewedTrade.buyTime() >= trade.buyTime() &&
+      reviewedTrade.buyTime() <= trade.sellTime() &&
+      reviewedTrade.sellTime() >= trade.sellTime();
   }
 }
