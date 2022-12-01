56a9d22c7ea8bd472fa92f7a5246ca93c7c80229
LukasBreuerDE <lukas.breuer2004@gmail.com>
2022-12-01 14:02:39 +0100

    Created NeuralNetworkFactory

---
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/neuralnetwork/NeuralNetworkFactory.java
@@ -0,0 +1,28 @@
+package de.lukasbreuer.stockalgorithm.core.neuralnetwork;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import de.lukasbreuer.stockalgorithm.core.dataset.HistoryIterator;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Singleton
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE, onConstructor = @__({@Inject}))
+public final class NeuralNetworkFactory {
+  @Inject @Named("networkSeed")
+  private final int seed;
+  @Inject @Named("networkEpochs")
+  private final int epochs;
+  @Inject @Named("networkLearningRate")
+  private final int learningRate;
+  @Inject @Named("networkDropoutRate")
+  private final int dropoutRate;
+  @Inject @Named("networkIterations")
+  private final int iterations;
+
+  public NeuralNetwork create(HistoryIterator historyIterator) {
+    return NeuralNetwork.create(seed, epochs, learningRate, dropoutRate,
+      iterations, -1, new int[0], -1, historyIterator);
+  }
+}
