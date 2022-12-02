f442af0848e100a1fc4522f6ba942cdd100bd9f0
DerCoderLukas <lukas.dercoder@gmail.com>
2022-12-02 17:42:33 +0100

    Created Evaluation and EvaluationFactory

---
+++ core/src/main/java/de/lukasbreuer/stockalgorithm/core/evaluation/Evaluation.java
@@ -0,0 +1,56 @@
+package de.lukasbreuer.stockalgorithm.core.evaluation;
+
+import com.google.common.collect.Maps;
+import de.lukasbreuer.stockalgorithm.core.dataset.StockDataset;
+import de.lukasbreuer.stockalgorithm.core.neuralnetwork.NeuralNetwork;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class Evaluation {
+  private final NeuralNetwork neuralNetwork;
+  private final StockDataset evaluationDataset;
+  private final int reviewPeriod;
+  private final int dayLongestReview;
+  private List<Integer> optimalSignals;
+  private List<Integer> determinedSignals;
+
+  public void analyse() {
+    var timePredictionAllocation = timePredictionAllocation();
+    optimalSignals = timePredictionAllocation.entrySet().stream()
+      .filter(entry -> entry.getValue() == 1)
+      .map(Map.Entry::getKey)
+      .collect(Collectors.toList());
+    determinedSignals = selectPromisingSignals(timePredictionAllocation);
+  }
+
+  private List<Integer> selectPromisingSignals(
+    Map<Integer, Float> timePredictionAllocation
+  ) {
+    return timePredictionAllocation.entrySet().stream()
+      .sorted(Map.Entry.<Integer, Float>comparingByValue().reversed())
+      .map(Map.Entry::getKey)
+      .collect(Collectors.toList());
+  }
+
+  private Map<Integer, Float> timePredictionAllocation() {
+    var allocation = Maps.<Integer, Float>newHashMap();
+    var rawDataset = evaluationDataset.raw();
+    for (var i = 1; i < evaluationDataset.size() - 1; i++) {
+      var entryTime = i + dayLongestReview + reviewPeriod;
+      allocation.put(entryTime, neuralNetwork.evaluate(entryTime, rawDataset.get(i)));
+    }
+    return allocation;
+  }
+
+  public List<Integer> optimalSignals() {
+    return List.copyOf(optimalSignals);
+  }
+
+  public List<Integer> determinedSignals() {
+    return List.copyOf(determinedSignals);
+  }
+}
