7bc2108f3d629eabf1881aa8930011d6c51108e1
LukasBreuerDE <lukas.breuer2004@gmail.com>
2022-12-21 13:29:00 +0100

    Added TradeExecution

---
+++ deploy/src/main/java/de/lukasbreuer/stockalgorithm/deploy/trade/TradeExecution.java
@@ -0,0 +1,8 @@
+package de.lukasbreuer.stockalgorithm.deploy.trade;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class TradeExecution {
+
+}
