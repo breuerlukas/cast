aa9577dd8b2f77da99c83da61f691e10833720b3
DerCoderLukas <lukas.dercoder@gmail.com>
2022-12-28 13:24:10 +0100

    Implemented model updating in ModelCommand

--- deploy/src/main/java/de/lukasbreuer/stockalgorithm/deploy/command/ModelCommand.java
+++ deploy/src/main/java/de/lukasbreuer/stockalgorithm/deploy/command/ModelCommand.java
@@ -13,18 +13,41 @@ public final class ModelCommand extends Command {
   private final ModelCollection modelCollection;
 
   private ModelCommand(Log log, ModelCollection modelCollection) {
-    super(log, "model", new String[] {"stock"});
+    super(log, "model", new String[] {"stock", "update <stock> " +
+      "<buyModelPath/sellModelPath/reviewPeriod/buyTradePredictionMinimum/" +
+      "sellTradePredictionMinimum> <value>"});
     this.modelCollection = modelCollection;
   }
 
   @Override
   public boolean execute(String[] arguments) {
-    if (arguments.length != 1) {
+    if (arguments.length == 0) {
       return false;
+    }
+    if (arguments[0].equalsIgnoreCase("update")) {
+      return executeUpdate(arguments);
     }
     var stock = arguments[0].toUpperCase();
     modelCollection.findByStock(stock, model -> printModel(stock, model));
     return true;
+  }
+
+  private boolean executeUpdate(String[] arguments) {
+    if (arguments.length != 4) {
+      return false;
+    }
+    var stock = arguments[1].toUpperCase();
+    var parameter = arguments[2];
+    var value = arguments[3];
+    modelCollection.findByStock(stock, model -> updateModel(stock, model, parameter, value));
+    return true;
+  }
+
+  private void updateModel(String stock, Model model, String parameter, String value) {
+    model.updateParameter(parameter, value);
+    modelCollection.updateModel(model, success ->
+      log().info("Successfully updated " + stock + "'s model parameter " + parameter +
+      " to " + value));
   }
 
   private void printModel(String stock, Model model) {
