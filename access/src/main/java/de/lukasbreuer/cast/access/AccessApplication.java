a7ccf8de4dddcf4e6d1c165b86774a5bb662f110
DerCoderLukas <lukas.dercoder@gmail.com>
2022-12-29 14:56:23 +0100

    Implemented AccessApplication

---
+++ access/src/main/java/de/lukasbreuer/cast/access/AccessApplication.java
@@ -0,0 +1,11 @@
+package de.lukasbreuer.cast.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessApplication {
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(AccessApplication.class, args);
+  }
+}
