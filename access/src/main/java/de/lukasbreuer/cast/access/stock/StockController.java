c845e90f8b6c370b41a04807709254ce922892a8
DerCoderLukas <lukas.dercoder@gmail.com>
2022-12-31 17:54:51 +0100

    Began implementation of StockController & StockConfiguration

---
+++ access/src/main/java/de/lukasbreuer/cast/access/stock/StockController.java
@@ -0,0 +1,65 @@
+package de.lukasbreuer.cast.access.stock;
+
+import com.google.common.collect.Maps;
+import de.lukasbreuer.cast.core.symbol.HistoryEntry;
+import de.lukasbreuer.cast.core.symbol.Symbol;
+import de.lukasbreuer.cast.core.trade.TradeType;
+import de.lukasbreuer.cast.deploy.model.Model;
+import de.lukasbreuer.cast.deploy.model.ModelCollection;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class StockController {
+  private final ModelCollection modelCollection;
+
+  @RequestMapping(path = "/stock/prices", method = RequestMethod.POST)
+  public CompletableFuture<Map<String, Object>> findPrices(
+    @RequestBody Map<String, Object> input
+  ) {
+    var completableFuture = new CompletableFuture<Map<String, Object>>();
+    new Thread(() -> findPrices(completableFuture, Symbol.createAndFetch((String) input.get("stock"))
+      .findPartOfHistory(Integer.parseInt((String) input.get("reviewPeriod"))))).start();
+    return completableFuture;
+  }
+
+  private void findPrices(
+    CompletableFuture<Map<String, Object>> futureResponse, List<HistoryEntry> data
+  ) {
+    var response = Maps.<String, Object>newHashMap();
+    response.put("prices", data.stream().map(HistoryEntry::close)
+      .collect(Collectors.toList()));
+    futureResponse.complete(response);
+  }
+
+  //TODO: PUT DL4J LIBRARY TO JAR CLASSPATH
+  @RequestMapping(path = "/stock/predictions", method = RequestMethod.POST)
+  public CompletableFuture<Map<String, Object>> findPredictions(
+    @RequestBody Map<String, Object> input
+  ) {
+    var completableFuture = new CompletableFuture<Map<String, Object>>();
+    modelCollection.findByStock((String) input.get("stock"),
+      model -> new Thread(() -> findPredictions(completableFuture, model,
+        TradeType.valueOf((String) input.get("tradeType")),
+        Integer.parseInt((String) input.get("reviewPeriod")))).start());
+    return completableFuture;
+  }
+
+  private void findPredictions(
+    CompletableFuture<Map<String, Object>> futureResponse, Model model,
+    TradeType tradeType, int reviewPeriod
+  ) {
+    model.initialize();
+    var response = Maps.<String, Object>newHashMap();
+    response.put("predictions", model.predict(tradeType, reviewPeriod));
+    futureResponse.complete(response);
+  }
+}
