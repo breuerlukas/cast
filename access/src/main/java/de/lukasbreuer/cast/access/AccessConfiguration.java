6a4b8702145692064fc471c134b41b726203a4c0
DerCoderLukas <lukas.dercoder@gmail.com>
2023-01-02 17:33:54 +0100

    Updated AccessConfiguration & StockConfiguration

--- access/src/main/java/de/lukasbreuer/cast/access/AccessConfiguration.java
+++ access/src/main/java/de/lukasbreuer/cast/access/AccessConfiguration.java
@@ -1,11 +1,30 @@
 package de.lukasbreuer.cast.access;
 
 import com.google.inject.Guice;
+import com.google.inject.Inject;
+import de.lukasbreuer.cast.core.log.Log;
+import de.lukasbreuer.cast.deploy.model.ModelCollection;
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AccessConfiguration {
+  @Inject
+  private Log log;
+  @Inject
+  private ModelCollection modelCollection;
+
+  @Bean
+  Log provideLog() {
+    return log;
+  }
+
+  @Bean
+  ModelCollection provideModelCollection() {
+    return modelCollection;
+  }
+
   @PostConstruct
   private void createModuleInjector() {
     var injector = Guice.createInjector(AccessModule.create());
--- access/src/main/java/de/lukasbreuer/cast/access/stock/StockConfiguration.java
+++ access/src/main/java/de/lukasbreuer/cast/access/stock/StockConfiguration.java
@@ -1,23 +1,12 @@
 package de.lukasbreuer.cast.access.stock;
 
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import de.lukasbreuer.cast.access.AccessModule;
-import de.lukasbreuer.cast.deploy.model.ModelCollection;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StockConfiguration {
-  @Inject
-  private ModelCollection modelCollection;
-
-  @Bean
-  ModelCollection provideModelCollection() {
-    return modelCollection;
-  }
-
   @PostConstruct
   private void createModuleInjector() throws Exception {
     var injector = Guice.createInjector(AccessModule.create());
